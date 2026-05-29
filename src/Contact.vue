98716f59b92fecbc4ac482959e28db15c36823bd
Votre Nom <votre.email@example.com>
2026-05-29 10:57:33 +0200

    Mise à jour de l'URL de l'API pour la production

--- src/Contact.vue
+++ src/Contact.vue
@@ -34,7 +34,7 @@ export default {
   methods: {
     async contact() {
       try {
-        const response = await fetch("http://localhost:3000/contact", {
+        const response = await fetch("https://organize-it-node.onrender.com/contact", {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({ nom: this.nom, email: this.email, message: this.messageContact })
