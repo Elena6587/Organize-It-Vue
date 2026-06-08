f3fbdded29d7872dc04c98e3c63a31e6476b9ccc
Votre Nom <votre.email@example.com>
2026-06-08 13:13:35 +0200

    Mise a jour des fichiers PHP de Adopt US

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-export default defineConfig({
-  base: './', // 👈 AJOUTE CETTE LIGNE ICI
-  plugins: [vue()],
+  transpileDependencies: true,
+  publicPath: './'
 })
